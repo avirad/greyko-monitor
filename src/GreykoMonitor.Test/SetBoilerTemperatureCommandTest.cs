15f7d6cb8093b68f3e3b858afedb1d3ab9df423d
avirad <avirad@mail.bg>
2016-12-23 01:56:18 +0200

    Update SetBoilerTemperatureCommand unit tests

--- src/GreykoMonitor.Test/SetBoilerTemperatureCommandTest.cs
+++ src/GreykoMonitor.Test/SetBoilerTemperatureCommandTest.cs
@@ -12,16 +12,6 @@ namespace GreykoMonitor.Test
     public class SetBoilerTemperatureCommandTest
     {
         [TestMethod]
-        public void SetBoilerTemperatureTo62()
-        {
-            ICommand command = new SetBoilerTemperatureCommand(62);
-            byte[] request = command.GetRequestData();
-
-            string hex = BitConverter.ToString(request).Replace("-", " ");
-            Assert.AreEqual(hex, "5A 5A 03 07 3F B9");
-        }
-
-        [TestMethod]
         public void SetBoilerTemperatureTo59()
         {
             ICommand command = new SetBoilerTemperatureCommand(59);
@@ -29,6 +19,26 @@ namespace GreykoMonitor.Test
 
             string hex = BitConverter.ToString(request).Replace("-", " ");
             Assert.AreEqual(hex, "5A 5A 03 07 3C BC");
+        }
+
+        [TestMethod]
+        public void SetBoilerTemperatureTo60()
+        {
+            ICommand command = new SetBoilerTemperatureCommand(60);
+            byte[] request = command.GetRequestData();
+
+            string hex = BitConverter.ToString(request).Replace("-", " ");
+            Assert.AreEqual(hex, "5A 5A 03 07 3D BB");
+        }
+
+        [TestMethod]
+        public void SetBoilerTemperatureTo62()
+        {
+            ICommand command = new SetBoilerTemperatureCommand(62);
+            byte[] request = command.GetRequestData();
+
+            string hex = BitConverter.ToString(request).Replace("-", " ");
+            Assert.AreEqual(hex, "5A 5A 03 07 3F B9");
         }
     }
 }
