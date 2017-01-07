b92d93344383b7f07c4aadff4ed33f476812b424
avirad <avirad@mail.bg>
2017-01-07 19:16:38 +0200

    Add parameters to GeneralInformationCommand Implement unit tests

--- src/GreykoMonitor.Test/GeneralInformationCommandTest.cs
+++ src/GreykoMonitor.Test/GeneralInformationCommandTest.cs
@@ -17,26 +17,37 @@ namespace GreykoMonitor.Test
             Assert.AreEqual(hex, "5A 5A 02 01 FD");
         }
 
+        /// <summary>
+        /// Tboiler increased from 53 to 54 and the burner turned the water pump ON.
+        /// </summary>
         [TestMethod]
-        public void GeneralInformationResponse1()
+        public void TurnCHPumpON()
         {
             ICommand command = new GeneralInformationCommand();
 
             byte[] response = new byte[] { 0x5A, 0x5A, 0x1D, 0x16, 0x14, 0x12, 0x4B, 0x57, 0x1B, 0x18, 0x1D, 0x09, 0x09, 0x0A, 0x0B, 0x8C,
                                            0x0D, 0x0E, 0x0F, 0x4C, 0x46, 0x92, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x99, 0x1A, 0x1B, 0xFA };
             command.ProcessResponseData(response);
+
             Assert.AreEqual(command.IsSuccessful, true);
-        }
+            Assert.AreEqual(((GeneralInformationCommand)command).Tset, 60);
+            Assert.AreEqual(((GeneralInformationCommand)command).Tboiler, 53);
+            Assert.AreEqual(((GeneralInformationCommand)command).Flame, 0);
+            Assert.AreEqual(((GeneralInformationCommand)command).Heater, false);
+            Assert.AreEqual(((GeneralInformationCommand)command).CHPump, false);
+            Assert.AreEqual(((GeneralInformationCommand)command).Fan, 0);
 
-        [TestMethod]
-        public void GeneralInformationResponse2()
-        {
-            ICommand command = new GeneralInformationCommand();
-
-            byte[] response = new byte[] { 0x5A, 0x5A, 0x1D, 0x16, 0x14, 0x12, 0x4B, 0x5C, 0x1B, 0x18, 0x1D, 0x09, 0x09, 0x0A, 0x0B, 0x8C,
-                                           0x0D, 0x0E, 0x0F, 0x4C, 0x47, 0x92, 0x13, 0x14, 0x1D, 0x16, 0x17, 0x18, 0x99, 0x1A, 0x1B, 0xEC };
+            response = new byte[] { 0x5A, 0x5A, 0x1D, 0x16, 0x14, 0x12, 0x4B, 0x5C, 0x1B, 0x18, 0x1D, 0x09, 0x09, 0x0A, 0x0B, 0x8C,
+                                    0x0D, 0x0E, 0x0F, 0x4C, 0x47, 0x92, 0x13, 0x14, 0x1D, 0x16, 0x17, 0x18, 0x99, 0x1A, 0x1B, 0xEC };
             command.ProcessResponseData(response);
+
             Assert.AreEqual(command.IsSuccessful, true);
+            Assert.AreEqual(((GeneralInformationCommand)command).Tset, 60);
+            Assert.AreEqual(((GeneralInformationCommand)command).Tboiler, 54);
+            Assert.AreEqual(((GeneralInformationCommand)command).Flame, 0);
+            Assert.AreEqual(((GeneralInformationCommand)command).Heater, false);
+            Assert.AreEqual(((GeneralInformationCommand)command).CHPump, true);
+            Assert.AreEqual(((GeneralInformationCommand)command).Fan, 0);
         }
     }
 }
--- src/GreykoMonitor/Communication/Commands/GeneralInformationCommand.cs
+++ src/GreykoMonitor/Communication/Commands/GeneralInformationCommand.cs
@@ -8,15 +8,24 @@ namespace GreykoMonitor.Communication.Commands
 {
     public class GeneralInformationCommand : CommandBase, ICommand
     {
-        protected const byte _expectedResponseLength = 32;
+        protected const byte _expectedResponseDataLength = 28;
+
         protected override byte _commandId { get; set; } = 0x01;
         protected override byte[] _requestData { get; set; }
         protected override byte[] _responseData { get; set; }
 
-        public byte Tboiler
-        {
-            get { return _responseData[30]; }
-        }
+        #region Public properties
+        public byte Tset { get { return _responseData[16]; } }
+
+        public byte Tboiler { get { return _responseData[17]; } }
+
+        public byte Flame { get { return _responseData[20]; } }
+
+        public bool Heater { get { return (_responseData[21] & (1 << 1)) != 0; } }
+        public bool CHPump { get { return (_responseData[21] & (1 << 3)) != 0; } }
+
+        public byte Fan { get { return _responseData[23]; } }
+        #endregion
 
         public override byte[] GetRequestData()
         {
@@ -31,7 +40,7 @@ namespace GreykoMonitor.Communication.Commands
             {
                 base.ProcessResponseData(response);
 
-                this.IsSuccessful = (response.Length == _expectedResponseLength);
+                this.IsSuccessful = (_responseData?.Length == _expectedResponseDataLength);
             }
             catch
             {
